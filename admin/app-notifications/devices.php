1858e1ef020e3debe45d0db7a63a49eaaf2aa8ce
Kinely Wait <kinley@genzballot.com>
2026-08-03 00:36:15 +0000

    devices.php: App Version header (employee table)

--- admin/app-notifications/devices.php
+++ admin/app-notifications/devices.php
@@ -209,6 +209,7 @@ require_once __DIR__ . '/../includes/header.php';
                         <th>Username</th>
                         <th>Email</th>
                         <th>Platform</th>
+                        <th>App Version</th>
                         <th>Registered</th>
                         <th>Last Active</th>
                     </tr>
